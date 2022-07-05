9de88a9e43e8af4ebc7ddcc591bb8778c71ae780
sansaoipb <sansaoipb@gmail.com>
2022-07-05 11:22:14 -0300

    Update status-ix-br.py
    Convertendo "prints" em "json.dumps"

--- status-ix-br.py
+++ status-ix-br.py
@@ -7,48 +7,44 @@
 
 from bs4 import BeautifulSoup
 import requests
+import json
 import re
 
-def request():
-    url = "https://status.ix.br"
+URL = "https://status.ix.br"
+
+
+def request(url):
     return requests.get(url)
+
 
 def parse_result(html):
     bs = BeautifulSoup(html, 'html.parser')
     ixlist = bs.find_all('ul', {'class': 'list-group components'})
 
-    data = {}
-    print("[")
+    data = []
     for ix in ixlist:
-            name = ix.find('strong').text.strip()
-            items = ix.find('div', {'class': 'group-items'}).find_all('li', {'class': 'list-group-item'})
-            ix_data = {}
-            for item in items:
-                status_div = item.find('div', {'class': 'pull-right'})
-                status = status_div.text.strip()
-                status_div.decompose()
-                item_name = item.text.strip()
-                if name == 'IX-br - Outras Localidades':
-                    ix_grupo = re.sub('.*- ', '', name)
-                    ix_nome = re.sub('IX.br ', '', item_name)
-                    ix_localizacao = ix_nome
-                else:
-                    ix_grupo = re.sub('IX.br ', '', name)
-                    ix_nome = re.sub(' -.*', '', item_name)
-                    ix_localizacao = re.sub('.*- IX.br ', '', item_name)
+        name = ix.find('strong').text.strip()
+        items = ix.find('div', {'class': 'group-items'}).find_all('li', {'class': 'list-group-item'})
+        for item in items:
+            status_div = item.find('div', {'class': 'pull-right'})
+            status = status_div.text.strip()
+            status_div.decompose()
+            item_name = item.text.strip()
+            if name == 'IX-br - Outras Localidades':
+                ix_grupo = re.sub('.*- ', '', name)
+                ix_nome = re.sub('IX.br ', '', item_name)
+                ix_localizacao = ix_nome
+            else:
+                ix_grupo = re.sub('IX.br ', '', name)
+                ix_nome = re.sub(' -.*', '', item_name)
+                ix_localizacao = re.sub('.*- IX.br ', '', item_name)
 
-                print("\t")
-                print("\t{")
-                print(f'\t\t"{"IX_GRUPO"}": "{ix_grupo}",')
-                print(f'\t\t"{"IX_LOCALIZACAO"}": "{ix_localizacao}",')
-                print(f'\t\t"{"IX_NOME"}": "{ix_nome}",')
-                print(f'\t\t"{"IX_STATUS"}": "{status}"')
-                print("\t},")
-    return data
+            ix_data = {"IX_GRUPO": ix_grupo, "IX_LOCALIZACAO": ix_localizacao, "IX_NOME": ix_nome, "IX_STATUS": status}
+            data.append(ix_data)
+
+    print(json.dumps(data, indent=4, ensure_ascii=False))
+
 
 if __name__ == '__main__':
-    response = request()
-
-print(parse_result(response.text))
-
-print("]")
+    response = request(URL)
+    parse_result(response.text)
